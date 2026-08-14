561d034ca552c07500206659756815b01c405cd3
Manan Jain <manansancheti02@gmail.com>
2026-08-14 11:26:08 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb+srv://<db_username>:manansancheti02_db_useronIDfdh0E4L2y06G@manan-cluster.gwo6w4x.mongodb.net/?appName=manan-cluster')
+
+const db=mongoose.connection;
+db.on("connected",()=>{
+    console.log("MongoDB connected successfully");
+});
+db.on("disconnected",()=>{
+    console.log("MongoDB disconnected ");
+});
+db.on("erroe",()=>{
+    console.log("MongoDB connection error:",error);
+});
+
+
+modeule.exports=db;
+
